d20864683d30998e0307e11073c59c0b23a1ee76
MasterUNG <phrombutr@gmail.com>
2018-01-03 10:29:21 +0700

    Choose Image

--- app/src/main/java/srongklod_bangtamruat/plantseconomic/fragment/CustomerShowFragment.java
+++ app/src/main/java/srongklod_bangtamruat/plantseconomic/fragment/CustomerShowFragment.java
@@ -1,5 +1,9 @@
 package srongklod_bangtamruat.plantseconomic.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,19 +12,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import srongklod_bangtamruat.plantseconomic.R;
 import srongklod_bangtamruat.plantseconomic.ServiceActivity;
+import srongklod_bangtamruat.plantseconomic.utility.MyAlert;
 
 /**
  * Created by masterung on 30/12/2017 AD.
  */
 
-public class CustomerShowFragment extends Fragment{
+public class CustomerShowFragment extends Fragment {
 
     //    Explicit
     private String[] customerStrings;
+    private Uri uri;
+    private ImageView imageView;
 
 
     public static CustomerShowFragment customerShowInstance(String[] customerStrings) {
@@ -39,14 +48,78 @@ public class CustomerShowFragment extends Fragment{
         super.onActivityCreated(savedInstanceState);
 
 //        Get Value From Activity
-        customerStrings = getArguments().getStringArray("Customer");
+        getValueFromArgument();
 
+//        Show Text
+        showText();
 
-
+//        Image Controller
+        imageController();
 
     }   // Main Method
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (resultCode == getActivity().RESULT_OK) {
+
+            //Show Image
+            try {
+
+                uri = data.getData();
+                Bitmap bitmap = BitmapFactory
+                        .decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.nomalDialog("Non Complete Choose Image",
+                    getString(R.string.message_choose_image));
+        }
+
+
+    }
+
+    private void imageController() {
+
+        imageView = getView().findViewById(R.id.imvAvata);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, getString(R.string.message_choose_image)),
+                        1);
+
+
+            }
+        });
+
+
+    }
+
+    private void showText() {
+
+        TextView nameTextView = getView().findViewById(R.id.txtName);
+        TextView lastNameTextView = getView().findViewById(R.id.txtLastName);
+        TextView phoneTextView = getView().findViewById(R.id.txtPhone);
+
+        nameTextView.setText(customerStrings[1]);
+        lastNameTextView.setText(customerStrings[0]);
+        phoneTextView.setText(customerStrings[2]);
+
+    }
+
+    private void getValueFromArgument() {
+        customerStrings = getArguments().getStringArray("Customer");
+    }
 
 
     @Nullable

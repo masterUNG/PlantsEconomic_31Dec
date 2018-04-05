89aeb7b2651719706618d77f4e480e04884e0428
MasterUNG <phrombutr@gmail.com>
2018-04-05 10:24:17 +0700

    Content Choose Image Success

--- app/src/main/java/srongklod_bangtamruat/plantseconomic/fragment/AddShopFragment.java
+++ app/src/main/java/srongklod_bangtamruat/plantseconomic/fragment/AddShopFragment.java
@@ -1,5 +1,9 @@
 package srongklod_bangtamruat.plantseconomic.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,18 +11,58 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import srongklod_bangtamruat.plantseconomic.R;
 
 public class AddShopFragment extends Fragment {
 
+    private boolean aBoolean = true;
+    private ImageView imageView;
+    private Uri uri;
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+//        Image Controller
+        imageController();
 
 
     }   // Main Method
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            uri = data.getData();
+            try {
+
+                Bitmap bitmap = BitmapFactory
+                        .decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void imageController() {
+        imageView = getView().findViewById(R.id.imvAddShop);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose Image"), 1);
+            }
+        });
+    }
 
     @Nullable
     @Override
